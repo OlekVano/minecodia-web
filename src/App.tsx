7354ac1ab5d1d2c4f43c1eb31d6ebb81a49b3a66
OlekVano <olekvano@gmail.com>
2023-01-17 13:33:37 +0000

    Added routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import { useEffect, useState } from 'react'
 import LoadingScreen from './components/LoadingScreen'
 
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+
 function App() {
   const [loading, setLoading] = useState(true)
 
@@ -10,7 +13,11 @@ function App() {
       <div asscroll-container='true' className='absolute left-0 top-0 max-h-full overflow-y-hidden'>
         {/* All content in the following div */}
         <div>
-          Content
+          <Router>
+            <Routes>
+              <Route index element={<Home />} />
+            </Routes>
+          </Router>
         </div>
       </div>
     </div>
