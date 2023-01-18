824d5e400f2e7b31d72ccea6020494deadeb3621
OlekVano <olekvano@gmail.com>
2023-01-18 00:50:20 +0000

    Added Button component

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,14 @@
+import { MouseEventHandler } from 'react'
+import styles from './index.module.scss'
+
+export const Button = ({ text='', func=()=>{} }: {text?: string, func?: Function}) => {
+  return (
+    <div role='button' className={styles.main} onClick={() => {new Audio('sounds/click.mp3').play(); func()}}>
+      <div className={styles.textWrapper}>
+        <div className={styles.text}>
+          {text}
+        </div>
+      </div>
+    </div>
+  )
+}
