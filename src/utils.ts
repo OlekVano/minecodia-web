1084cdd6adb613b54fa68107329e10c703a52d5e
OlekVano <olekvano@gmail.com>
2023-01-18 22:49:43 +0000

    Added getRandNum and getRandArrItem functions to the utils.ts file

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,11 @@
+export function getRandNum(max: number): number {
+  return Math.floor(Math.random() * max)
+}
+    
+export function getRandArrItem<T>(arr: T[]): T {
+  return arr[getRandNum(arr.length)]
+}
+
 export function sleep(ms: number) {
-    return new Promise(resolve => setTimeout(resolve, ms));
+  return new Promise(resolve => setTimeout(resolve, ms))
 }
