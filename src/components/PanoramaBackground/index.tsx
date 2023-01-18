45a5296d08dc2b355c706a3cc176452f7ed15c59
OlekVano <olekvano@gmail.com>
2023-01-18 22:50:07 +0000

    Added PanoramaBackground component

---
+++ src/components/PanoramaBackground/index.tsx
@@ -0,0 +1,10 @@
+import styles from './index.module.scss'
+
+export default function PanoramaBackground({ bgImage }: {bgImage: string}) {
+  return (
+    <div className='fixed overflow-x-hidden max-w-full'>
+      <div className={`${styles.main} aspect-[8/1] h-screen block bg-contain bg-repeat-x`} style={{backgroundImage: `url(${bgImage})`}}>
+      </div>
+    </div>
+  )
+}
