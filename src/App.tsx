3646d8b1229c3d771db52c8fc3fac17c9c0f97d4
OlekVano <olekvano@gmail.com>
2023-01-19 10:31:46 +0000

    Refactored firebase code and created a working sign in / sign out system

--- src/App.tsx
+++ src/App.tsx
@@ -6,42 +6,12 @@ import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import SignIn from './pages/SignIn'
 
-// Import the functions you need from the SDKs you need
-
-import firebase from 'firebase/compat/app';
-
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-
-function firstTimeOnWebsite() {
-  return localStorage.getItem('visited') == null
-}
-
 function App() {
   const navigate = useNavigate()
   const [loading, setLoading] = useState(true)
 
   useEffect(() => {
-    console.log(localStorage)
-    console.log(localStorage.getItem('visited'))
-
-    const firebaseConfig = {
-      apiKey: "AIzaSyAgyTWyf-uON4n-E_m16YEp-ktKuukUQ-4",
-      authDomain: "minecodia.firebaseapp.com",
-      projectId: "minecodia",
-      storageBucket: "minecodia.appspot.com",
-      messagingSenderId: "36714364576",
-      appId: "1:36714364576:web:f4cce0cd1274f6fdd9d3c5",
-      measurementId: "G-F90TGS14GW"
-    };
-
-    const app = firebase.initializeApp(firebaseConfig);
-    console.log('Firebase initialized')
     
-    // if (firstTimeOnWebsite()) navigate('/sign-in')
   }, [])
 
   return (
--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,43 +1,61 @@
-import { useEffect } from "react"
-
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/auth';
-import 'firebase/compat/firestore';
+import { useEffect, useState } from "react"
 import { Button } from "../components/Button";
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { auth } from '../firebase-setup'
+import { GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth'
 
 export default function SignIn({ loading }: any) {
-  function signIn() {
-    const provider = new firebase.auth.GoogleAuthProvider()
-    firebase.auth()
-  .signInWithPopup(provider)
-  .then((result) => {
-    /** @type {firebase.auth.OAuthCredential} */
-    // var credential = result.credential 
+  const [user] = useAuthState(auth)
 
-    // This gives you a Google Access Token. You can use it to access the Google API.
-    // var token = credential.accessToken;
-    // The signed-in user info.
-    // var user = result.user;
-    // // ...
-  }).catch((error) => {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // The email of the user's account used.
-    var email = error.email;
-    // The firebase.auth.AuthCredential type that was used.
-    var credential = error.credential;
-    // ...
-  });
+  function signIn() {
+    const provider = new GoogleAuthProvider()
+    
+    signInWithPopup(auth, provider)
+    .then((result) => {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      if (credential == null) return
+      const token = credential.accessToken;
+      // The signed-in user info.
+      const user = result.user;
+      // ...
+    })
+    .catch((error) => {
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // The email of the user's account used.
+      const email = error.customData.email;
+      // The AuthCredential type that was used.
+      const credential = GoogleAuthProvider.credentialFromError(error);
+      // ...
+    })
   }
 
   return (
-    <div className='w-screen h-screen bg-[url("../public/images/dirt-bg.webp")] font-["minecraft"] bg-cover bg-center grid place-items-center'>
+    <>
+    {
+      loading ? <></>
+      : 
+      <div className='w-screen h-screen bg-[url("../public/images/dirt-bg.webp")] font-["minecraft"] bg-cover bg-center grid place-items-center'>
       <div className={`gap-10 flex duration-1000 delay-1000 flex-wrap justify-center ${loading ? 'opacity-0' : ''}`}>
-        <div className='w-full text-center  text-white' style={{wordSpacing: '3.5px'}}>Signing in allows posting, commenting and having a profile...</div>
-        <Button text='Sign In' func={signIn} />
-        <Button text='Guest Mode' />
+        {
+          user ?
+          <>
+            <div className='w-full text-center  text-white' style={{wordSpacing: '3.5px'}}>You are signed in</div>
+            <Button text='Explore' />
+            <Button text='Sign Out' func={() => signOut(auth)} />
+          </>
+          :
+          <>
+            <div className='w-full text-center  text-white' style={{wordSpacing: '3.5px'}}>Signing in allows posting, commenting and having a profile...</div>
+            <Button text='Sign In' func={signIn} />
+            <Button text='Guest Mode' />
+          </>
+        }
       </div>
     </div>
+    }
+    </>
   )
 }
