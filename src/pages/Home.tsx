51a5465a9d27d47736c45f4b4f5c422f680e7003
OlekVano <olekvano@gmail.com>
2023-01-20 22:24:31 +0000

    Added Asscroll to the Home.tsx component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,6 +5,8 @@ import SplashText from '../components/SplashText'
 import { useNavigate } from 'react-router-dom'
 import { getRandArrItem } from '../utils'
 
+import ASScroll from '@ashthornton/asscroll'
+
 export default function Home({ loading }: {loading: boolean}) {
   const navigate = useNavigate()
 
@@ -18,26 +20,37 @@ export default function Home({ loading }: {loading: boolean}) {
   const [splashText, setSplashText] = useState('')
 
   useEffect(() => {
+    if (loading) return
     setSplashText(getRandArrItem(splashTexts))
-  }, [])
+
+    const asscroll = new ASScroll()
+    asscroll.enable()
+    return () => {
+      asscroll.disable()
+    }
+  }, [loading])
 
   return (
     <div>
       <PanoramaBackground bgImage='images/panorama-bg.jpg' />
-      <div className={`absolute top-0 left-0 flex flex-col justify-evenly items-center duration-1000 delay-1000 select-none min-h-screen  ${loading ? 'opacity-0': ''}`}>
-        <div className='w-full sm:w-2/3 lg:w-1/2 h-full relative'>
-          <img src='/images/logo.png'></img>
-          <div className='absolute bottom-1/4 right-0'>
-            <SplashText text={splashText} />
+      <div asscroll-container='true' className='absolute top-0 left-0'>
+        <div>
+        <div className={`flex flex-col justify-evenly items-center select-none min-h-screen ${loading ? 'opacity-0 hidden': ''}`}>
+          <div className='w-full sm:w-2/3 lg:w-1/2 h-full relative'>
+            <img src='/images/logo.png'></img>
+            <div className='absolute bottom-1/4 right-0'>
+              <SplashText text={splashText} />
+            </div>
+          </div>
+          <h1 className='text-2xl uppercase text-center'>
+            The first social media platform designed for Minecraft fans!
+          </h1>
+          <div className='flex justify-center gap-x-16 gap-y-8 w-full max-w-2xl flex-wrap'>
+            <Button text='Try it' func={() => navigate('/sign-in')} />
+            <Button text='Learn more' />
           </div>
         </div>
-        <h1 className='text-2xl uppercase text-center'>
-          The first social media platform designed for Minecraft fans!
-        </h1>
-        <div className='flex justify-center gap-x-16 gap-y-8 w-full max-w-2xl flex-wrap'>
-          <Button text='Try it' func={() => navigate('/sign-in')} />
-          <Button text='Learn more' />
-        </div>
+      </div>
       </div>
     </div>
   )
