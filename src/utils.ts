1195e6ded36081d430499c06f8a74ead01e6d2f8
OlekVano <olekvano@gmail.com>
2023-01-24 08:36:57 +0000

    Added a fetchUserById function to utils.ts

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,7 @@
+import { UserProfile } from './types'
+
+import { User as AuthUser } from 'firebase/auth'
+
 export function getRandNum(max: number): number {
   return Math.floor(Math.random() * max)
 }
@@ -8,4 +12,11 @@ export function getRandArrItem<T>(arr: T[]): T {
 
 export function sleep(ms: number): Promise<void> {
   return new Promise(resolve => setTimeout(resolve, ms))
+}
+
+export async function fetchUserById(id: string, user: AuthUser): Promise<UserProfile | undefined> {
+  const token = await user.getIdToken()
+  const res = await fetch(`${process.env.REACT_APP_API_URL}/users/${id}`, {headers: new Headers({'Authorization': `Bearer ${token}`})})
+  if (res.status !== 200) return undefined
+  return await res.json()
 }
