2b1e7b36dec0b81661dffada45c337376109e1d6
OlekVano <olekvano@gmail.com>
2023-01-25 10:24:28 +0000

    Added smooth background transition to the avatar

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -8,7 +8,7 @@ type Props = {
 
 export default function Avatar({ background, skin, nickname }: Props) {
   return (
-    <div id='skin-container' style={{backgroundImage: `url(${background})`}} className={`pointer-events-none bg-cover bg-bottom w-full h-full grid place-items-center`}>
+    <div id='skin-container' style={{backgroundImage: `url(${background})`}} className={`transition-[background] duration-500 pointer-events-none bg-cover bg-bottom w-full h-full grid place-items-center`}>
       <SkinCanvas nickname={nickname} containerId='skin-container' skinImg={skin}  />
     </div>
   )
