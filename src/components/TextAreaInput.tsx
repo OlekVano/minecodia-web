a7fc942a0b3b36c5797c70e46cd6f8ceecea638b
OlekVano <olekvano@gmail.com>
2023-01-31 17:03:27 +0000

    Made the text input scrollable

--- src/components/TextAreaInput.tsx
+++ src/components/TextAreaInput.tsx
@@ -8,6 +8,6 @@ type Props = {
 
 export default function TextAreaInput({ onChangeFunc=()=>{}, id='', value='' }: Props) {
   return (
-    <textarea id={id} className='overflow-hidden w-full p-2 bg-opacity-50 bg-black border-2 border-gray-300 h-[350px] resize-none outline-none' onChange={onChangeFunc} value={value} />
+    <textarea id={id} className='asscroll-block w-full p-2 bg-opacity-50 bg-black border-2 border-gray-300 h-[350px] resize-none outline-none' onChange={onChangeFunc} value={value} />
   )
 }
