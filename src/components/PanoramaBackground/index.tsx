63bc0795e0a60d23874863d03daf453095186cde
OlekVano <olekvano@gmail.com>
2023-02-02 23:38:14 +0000

    Fixed a bug with smoothscroll

--- src/components/PanoramaBackground/index.tsx
+++ src/components/PanoramaBackground/index.tsx
@@ -8,7 +8,7 @@ type Props = {
 
 export default function PanoramaBackground({ bgImage }: Props) {
   return (
-    <div className='fixed overflow-x-hidden max-w-full'>
+    <div className='fixed overflow-x-hidden max-w-full -z-50'>
       <div className={`${styles.main} aspect-[8/1] h-screen block bg-contain bg-repeat-x`} style={{backgroundImage: `url(${bgImage})`}}>
       </div>
     </div>
