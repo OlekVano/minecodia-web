38867653b48e34d47a5b924c9cb01e9637d8cdcb
OlekVano <olekvano@gmail.com>
2023-02-13 14:09:19 +0000

    Fixed background jumps on mobile

--- src/components/DirtBackground.tsx
+++ src/components/DirtBackground.tsx
@@ -1,6 +1,6 @@
 export default function DirtBackground() {
   return (
-    <div className='fixed overflow-x-hidden h-full w-full -z-50'>
+    <div className='fixed overflow-x-hidden h-screen w-full -z-50'>
       <div className='h-full block bg-cover bg-[url("../public/images/dirt-bg.webp")] bg-center'></div>
     </div>
   )
--- src/components/PanoramaBackground/index.tsx
+++ src/components/PanoramaBackground/index.tsx
@@ -8,7 +8,7 @@ type Props = {
 
 export default function PanoramaBackground({ bgImage }: Props) {
   return (
-    <div className='fixed h-full overflow-x-hidden max-w-full -z-50'>
+    <div className='fixed h-screen overflow-x-hidden max-w-full -z-50'>
       <div className={`${styles.main} aspect-[8/1] h-full block bg-contain bg-repeat-x`} style={{backgroundImage: `url(${bgImage})`}}>
       </div>
     </div>
